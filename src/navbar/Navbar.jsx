4157627b3dc6db91e0d85c4e6cab67ded8c253b7
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-29 21:48:50 +0500

    continus

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
-
+import './Navbar.css'
 
 function Navbar() {
   return (
     <>
-    <div>
-        <h1>Abdul Rehman</h1>
-    </div>
-    <div>
+    
+    <nav className='navbar'>
+
+      <h1 className="logo-text">
+  &lt;Abdul Rehman /&gt;
+</h1>
+
+      <div className='nav-links'>
         <Link to="/Home">Home</Link>
         <Link to="/About">About</Link>
         <Link to="/Skills">Skills</Link>
         <Link to="/Projects">Projects</Link>
         <Link to="/Certifications">Certifications</Link>
         <Link to="/Contact">Contact</Link>
-        
-    </div>
+      </div>
+
+    </nav>
 
     </>
   )
