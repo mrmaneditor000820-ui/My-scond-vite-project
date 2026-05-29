12ba7c7b4e43ba89cb3151839f5a998bfe50c878
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-29 21:14:46 +0500

    router some problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './navbar/Navbar';
+import Router from './Router';
 
 function App() {
   return (
     <>
-  <Navbar/>
+  <Router/>
     </>
   )
 }
---
+++ src/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './homepage/Home'
+import Navbar from './navbar/Navbar';
+
+
+function Router() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default Router;
