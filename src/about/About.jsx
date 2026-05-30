9d7e23a5aa23ca5d1a472140f89e200a470fcca3
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 15:44:14 +0500

    about lorem text deletd

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque! Quas placeat quis ad impedit quasi provident. Accusantium, tenetur optio. Eligendi, perferendis hic cumque quia sed aut obcaecati aperiam provident dolores. Quis magni ea iste soluta recusandae error perferendis, at explicabo illum provident adipisci labore vel accusantium beatae necessitatibus harum quos quas dolorem veritatis optio! Adipisci vel reprehenderit eaque velit quibusdam, exercitationem tempora cupiditate ullam dolore voluptas illum accusantium, assumenda sit sint enim culpa voluptates quae nam consequatur quasi! Veritatis asperiores magni eligendi! Velit consequatur ea, voluptas sed porro rem enim odit nesciunt amet est ex nihil praesentium laborum doloremque eos quae minus omnis ut. Incidunt libero optio cum reprehenderit dolor dolore nostrum, dolorem error praesentium hic nihil, consequuntur vel quibusdam quia dolorum consequatur perferendis magni accusantium nemo facere minima nobis ea vitae? Porro iste possimus doloremque voluptates perferendis libero culpa in necessitatibus doloribus, delectus consequatur? Soluta vero delectus eveniet accusantium fugiat, ab eius molestiae deserunt iste aut sunt ullam eligendi autem voluptates praesentium porro explicabo. Rem, numquam. Velit numquam quae modi mollitia quibusdam. Consequuntur molestiae deleniti itaque? Eligendi laborum doloremque et libero tempora, repellendus iusto commodi tempore fugit eum ex impedit consequatur harum tenetur blanditiis nam? Accusantium, repellendus praesentium!
-
-
-
-
-      <h1>about</h1>
-    </div>
+    <div>About2</div>
   )
 }
 
