2d5e3283884e312e09ce596676646d804d3dbb34
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 10:53:00 +0500

    navbar some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './navbar/Navbar';
 import Router from './Router';
+import Home from './homepage/Home';
 
 function App() {
   return (
     <>
   <Router/>
+  <Home/>
     </>
   )
 }
