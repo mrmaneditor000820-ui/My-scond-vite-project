b9d257fc7a604911c3eb58b835a45d9f8adde7c9
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 15:53:30 +0500

    home page structure creating

--- src/certifications/Certifications.jsx
+++ src/certifications/Certifications.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Certifications() {
   return (
-    <div>Certifications</div>
+    <div></div>
   )
 }
 
--- src/homepage/Home.jsx
+++ src/homepage/Home.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 function Home() {
   return (
+    <>
     <div>
     <h1>Hi, I'm Abdul Rehman</h1>
     </div>
+    <div>
+      <p>
+        I’m Abdul Rehman, a passionate Frontend Developer specializing in React.js and modern web technologies. I enjoy building responsive, user-friendly, and visually appealing web applications that provide seamless user experiences. Through hands-on projects, I have gained experience working with React, JavaScript, Context API, and modern UI development practices. I am constantly learning new technologies, improving my problem-solving skills, and creating projects that transform ideas into real-world solutions. My goal is to grow as a Full-Stack Developer while contributing to impactful and innovative digital products.
+      </p>
+    </div>
+    <div>
+      <button>
+       Contact Me
+      </button>
+       <button>
+       View Projects
+      </button>
+    </div>
+    
+    </>
   )
 }
 
