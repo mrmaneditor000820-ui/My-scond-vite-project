b85d5a0fa0c5242d4ebc168caa9e2cf39b25c2bf
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 15:46:28 +0500

    routing unlinked

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './homepage/Home'
-import Navbar from './navbar/Navbar';
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import Home from './homepage/Home'
+// import Navbar from './navbar/Navbar';
 
 
-function Router() {
-  return (
-    <>
-    <Navbar/>
-    <Routes>
-        <Route path="/Home" element={<Home/>}/>
-    </Routes>
-    </>
-  )
-}
+// function Router() {
+//   return (
+//     <>
+//     <Navbar/>
+//     <Routes>
+//         <Route path="/Home" element={<Home/>}/>
+//     </Routes>
+//     </>
+//   )
+// }
 
-export default Router;
+// export default Router;
