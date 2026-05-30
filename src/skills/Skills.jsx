5b9d68013eff19240730d17dad967eeceb152d6e
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 17:17:29 +0500

    skills image added

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div></div>
+   <>
+   <div>
+    <h1>Skills & Technologics</h1>
+   </div>
+   <div>
+    
+   </div>
+   </>
   )
 }
 
