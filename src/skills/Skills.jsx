7b359770f779e83ea8f7309f9def1d5ec758fe44
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 19:05:57 +0500

    skills file id and classes adding

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,42 +1,72 @@
 import React from 'react'
 import javascript from '../assets/js.png'
-import typescript from '../assets/assets/type.png'
-import html from '../assets/assets/html.jpeg'
-import react from '../assets/assets/react.png'
-import Node from '../assets/assets/node.png'
-import Fire from '../assets/assets/fire.png'
-import Css from '../assets/assets/css.png'
-import Boot from '../assets/assets/boot.jpeg'
-import Ghithub from '../assets/assets/ghithub.png'
+import typescript from '../assets/type.png'
+import html from '../assets/html.jpeg'
+import react from '../assets/react.png'
+import Node from '../assets/node.png'
+import Fire from '../assets/fire.png'
+import Css from '../assets/css.png'
+import Boot from '../assets/boot.jpeg'
+import Ghithub from '../assets/ghithub.png'
+import './Skills.css'
 
 function Skills() {
   return (
    <>
-   <div>
-    <h1>Skills & Technologics</h1>
+   <div className="skills-heading">
+    <h1>Skills & Technologies</h1>
    </div>
-   <div>
-    <img src={javascript} alt="JavaScript" />
-    <p>JavaScript</p>
-     <img src={typescript} alt="TypeScript" />
-    <p>TypeScript</p>
-     <img src={html} alt="HTML" />
-    <p>HTML</p>
-     <img src={react} alt="React" />
-    <p>React</p>
-     <img src={Node} alt="Node.js" />
-    <p>Node.js</p>
-     <img src={Fire} alt="Firebase" />
-    <p>Firebase</p>
-     <img src={Css} alt="CSS" />
-    <p>CSS</p>
-     <img src={Boot} alt="Bootstrap" />
-    <p>Bootstrap</p>
-     <img src={Ghithub} alt="GitHub" />
-    <p>GitHub</p>
+
+   <div className="skills-container">
+
+    <div className="skill-card">
+      <img src={javascript} alt="JavaScript" />
+      <p>JavaScript</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={typescript} alt="TypeScript" />
+      <p>TypeScript</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={html} alt="HTML" />
+      <p>HTML</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={react} alt="React" />
+      <p>React</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={Node} alt="Node.js" />
+      <p>Node.js</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={Fire} alt="Firebase" />
+      <p>Firebase</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={Css} alt="CSS" />
+      <p>CSS</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={Boot} alt="Bootstrap" />
+      <p>Bootstrap</p>
+    </div>
+
+    <div className="skill-card">
+      <img src={Ghithub} alt="GitHub" />
+      <p>GitHub</p>
+    </div>
+
    </div>
     </>
   )
 }
 
-export default Skills;
+export default Skills
