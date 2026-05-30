2f491c5a0b5bc52bb15762ba31d2cbd21c80d2f0
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 16:29:50 +0500

    auto right added

--- src/homepage/Home.jsx
+++ src/homepage/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Home.css'
 import MyImage from '../assets/My.jpeg'
+import { TypeAnimation } from 'react-type-animation';
+
 
 function Home() {
   return (
@@ -9,11 +11,23 @@ function Home() {
       <div className="home-content">
 
         <h1>
-          Hi, I'm <span>Abdul Rehman</span>
-        </h1>
+  Hi, I'm <span>Abdul Rehman</span>
+</h1>
 
-        <h3>React Frontend Developer</h3>
-
+<h2>
+  <TypeAnimation
+    sequence={[
+      'React Developer',
+      2000,
+      'Frontend Developer',
+      2000,
+      'Problem Solver',
+      2000,
+    ]}
+    speed={50}
+    repeat={Infinity}
+  />
+</h2>
         <p>
           I’m  Frontend Developer specializing in React.js and modern web technologies. I enjoy building responsive, user-friendly, and visually appealing web applications that provide seamless user experiences. Through hands-on projects, I have gained experience working with React, JavaScript, Context API, and modern UI development practices. I am constantly learning new technologies, improving my problem-solving skills, and creating projects that transform ideas into real-world solutions. My goal is to grow as a Full-Stack Developer while contributing to impactful and innovative digital products.
         </p>
