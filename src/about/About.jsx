4e267b67f7f726ffdf910b2666c4c8c595ff87f6
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 16:54:25 +0500

    about section structure creating

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 function About() {
   return (
-    <div></div>
+   <>
+   <div>
+    <h1>About Me</h1>
+   </div>
+   <div>
+    <h3>
+      Who I Am
+    </h3>
+    <h5>
+      I’m  Frontend Developer specializing in React.js and modern web technologies. I enjoy building responsive, user-friendly, and visually appealing web applications that provide seamless user experiences. Through hands-on projects, I have gained experience working with React, JavaScript, Context API, and modern UI development practices. I am constantly learning new technologies, improving my problem-solving skills, and creating projects that transform ideas into real-world solutions. My goal is to grow as a Full-Stack Developer while contributing to impactful and innovative digital products.
+    </h5>
+   </div>
+   <div>
+    <h3>Education</h3>
+    <h4>Govt National College</h4>
+    <p>Pre Engeneering</p>
+    <p>2024---2025</p>
+   </div>
+   <div>
+    <h3>Mordern Web And App Development</h3>
+    <p>Sylani Mass It Training</p>
+   </div>
+   </>
   )
 }
 
-export default About
+export default About;
