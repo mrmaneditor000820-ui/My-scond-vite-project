ef5953d86f5aa68bace1764ed31bd96ede288fb4
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 19:13:36 +0500

    navbar project file location changed

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -16,8 +16,8 @@ function Navbar() {
   <a href="#home">Home</a>
   <a href="#about">About</a>
   <a href="#skills">Skills</a>
-  <a href="#certifications">Certifications</a>
   <a href="#projects">Projects</a>
+  <a href="#certifications">Certifications</a>
   <a href="#contact">Contact</a>
 
       </div>
