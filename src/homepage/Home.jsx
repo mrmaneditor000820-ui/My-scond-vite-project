6bd126d79bf61bf985922c831da053bf55d173f7
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 16:09:02 +0500

    home classes added

--- src/homepage/Home.jsx
+++ src/homepage/Home.jsx
@@ -1,33 +1,49 @@
 import React from 'react'
+import './Home.css'
+import MyImage from '../assets/My.jpeg'
 
 function Home() {
   return (
-    <>
-    <div>
-    <h1>Hi, I'm Abdul Rehman</h1>
-    </div>
-    <div>
-      <p>
-        I’m Abdul Rehman, a passionate Frontend Developer specializing in React.js and modern web technologies. I enjoy building responsive, user-friendly, and visually appealing web applications that provide seamless user experiences. Through hands-on projects, I have gained experience working with React, JavaScript, Context API, and modern UI development practices. I am constantly learning new technologies, improving my problem-solving skills, and creating projects that transform ideas into real-world solutions. My goal is to grow as a Full-Stack Developer while contributing to impactful and innovative digital products.
-      </p>
-    </div>
-    <div>
-      <button>
-       Contact Me
-      </button>
-       <button>
-       View Projects
-      </button>
-    </div>
-    <div>
-      <a href="https://www.linkedin.com/feed/">linkedin</a>
-      <a href="https://github.com/mrmaneditor000820-ui">Ghithub</a>
-    </div>
-    <div>
-      <img src="" alt="" />
-    </div>
-    </>
+    <section className="home">
+
+      <div className="home-content">
+
+        <h1>
+          Hi, I'm <span>Abdul Rehman</span>
+        </h1>
+
+        <h3>React Frontend Developer</h3>
+
+        <p>
+          I’m Abdul Rehman, a passionate Frontend Developer specializing in
+          React.js and modern web technologies. I enjoy building responsive,
+          user-friendly, and visually appealing web applications that provide
+          seamless user experiences.
+        </p>
+
+        <div className="btn-group">
+          <button className="primary-btn">Contact Me</button>
+          <button className="secondary-btn">View Projects</button>
+        </div>
+
+        <div className="social-links">
+          <a href="https://www.linkedin.com/feed/" target="_blank">
+            LinkedIn
+          </a>
+
+          <a href="https://github.com/mrmaneditor000820-ui" target="_blank">
+            GitHub
+          </a>
+        </div>
+
+      </div>
+
+      <div className="home-image">
+        <img src={MyImage} alt="Abdul Rehman" />
+      </div>
+
+    </section>
   )
 }
 
-export default Home
+export default Home;
