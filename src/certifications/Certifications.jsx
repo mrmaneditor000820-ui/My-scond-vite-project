b9f65ca77dfd53709a0d19cb37ec7395b2d35696
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 15:45:32 +0500

    another files lorem text deleted

--- src/certifications/Certifications.jsx
+++ src/certifications/Certifications.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 function Certifications() {
   return (
-    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, in consequuntur explicabo natus nemo provident exercitationem laudantium vero numquam, harum ipsum ad quod pariatur omnis quam iure. Aliquid excepturi nobis atque itaque reiciendis voluptatibus maiores reprehenderit tenetur, animi quia cumque explicabo? Doloremque unde debitis possimus. Corporis fugiat, provident quod dolorem alias soluta quas eos cupiditate deleniti praesentium ullam reprehenderit dignissimos obcaecati aliquid numquam, fuga dolorum! Culpa unde consequatur alias, provident nulla ipsum dicta voluptatibus iste velit tempore nesciunt omnis non fugiat asperiores facilis, error animi placeat porro. Reiciendis sit saepe minus magnam, excepturi similique optio quia rem repellendus voluptas, repellat sapiente alias ratione explicabo ipsum aliquid suscipit quibusdam, culpa architecto quod mollitia. Optio, doloremque saepe magni voluptas ex ab accusantium ipsam esse perspiciatis exercitationem commodi placeat? Sed distinctio officiis voluptatem voluptas corporis. Quod aspernatur temporibus obcaecati vel, delectus, labore alias sit nisi corrupti illo animi dignissimos voluptatum qui aliquid eveniet. Culpa omnis debitis distinctio assumenda, adipisci rem quas qui consequuntur, dolorum cum quibusdam et delectus amet minus quae. In ea maiores placeat corporis reiciendis ducimus ratione unde iusto rerum! Fugiat cumque neque quidem voluptate reiciendis expedita aspernatur. Eaque maiores nesciunt quasi, cum dolorem dignissimos quibusdam voluptas rerum neque ipsum perspiciatis?
-
-
-      
-    </div>
+    <div>Certifications</div>
   )
 }
 
--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aspernatur maxime accusantium ex sapiente rem, necessitatibus amet doloribus, adipisci id, minus itaque explicabo? Saepe asperiores et similique, qui harum dignissimos natus sequi obcaecati facilis labore distinctio quia inventore quis sint a? Voluptates deserunt praesentium ducimus consequuntur magnam earum ad id aliquid quae quas consectetur est libero perferendis neque voluptatem pariatur tempora, ratione assumenda modi eum! Harum sint commodi ratione voluptas atque laborum autem dignissimos, fuga est perferendis iste sunt cumque reiciendis molestiae, ullam quas eligendi quasi praesentium aspernatur quis velit vero unde iure! Eos porro nam possimus magnam doloribus? Ex.
-
-
-
-      <h1>projects</h1>
-    </div>
+    <div>Projects</div>
   )
 }
 
