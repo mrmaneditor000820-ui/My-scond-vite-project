666f68433c267348b84c6284d8f9262827272ea6
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-30 17:31:49 +0500

    files name change

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Javascript from '../assets/download.png'
+import Typescript from '../assets/assets/download (1).png'
 
 function Skills() {
   return (
@@ -7,7 +9,26 @@ function Skills() {
     <h1>Skills & Technologics</h1>
    </div>
    <div>
-    
+    <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Typescript} alt="TypeScript" />
+    <p>TypeScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
+     <img src={Javascript} alt="JavaScript" />
+    <p>JavaScript</p>
    </div>
    </>
   )
