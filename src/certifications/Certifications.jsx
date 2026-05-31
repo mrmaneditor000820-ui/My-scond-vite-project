91789e3032e535032930f5a814203b48d913918e
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-31 07:27:01 +0500

    continus

--- src/certifications/Certifications.jsx
+++ src/certifications/Certifications.jsx
@@ -21,6 +21,11 @@ function Certifications() {
       <h5>Course By Cisco Networking Academy</h5>
       <p>"Completed a professional certification in Web Development Foundations by Cisco Networking Academy, covering semantic HTML5 structure, web accessibility standards, clean coding practices, and core document object model layout."</p>
     </div>
+       <div>
+      <h3>Certificate OF HTML</h3>
+      <h5>Course By Cisco Networking Academy</h5>
+      <p>"Completed a professional certification in Web Development Foundations by Cisco Networking Academy, covering semantic HTML5 structure, web accessibility standards, clean coding practices, and core document object model layout."</p>
+    </div>
     </>
   )
 }
