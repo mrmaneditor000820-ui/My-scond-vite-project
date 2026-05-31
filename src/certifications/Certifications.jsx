8722b24050e55a9727ed1aaeb8df3dc493555401
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-31 07:26:28 +0500

    certification structure creating

--- src/certifications/Certifications.jsx
+++ src/certifications/Certifications.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Certifications() {
   return (
-    <div></div>
+    <>
+    <div>
+      <h1>Certifications & Achievements</h1>
+    </div>
+    <div>
+      <h3>Certificate OF HTML</h3>
+      <h5>Course By Cisco Networking Academy</h5>
+      <p>"Completed a professional certification in Web Development Foundations by Cisco Networking Academy, covering semantic HTML5 structure, web accessibility standards, clean coding practices, and core document object model layout."</p>
+    </div>
+       <div>
+      <h3>Certificate OF HTML</h3>
+      <h5>Course By Cisco Networking Academy</h5>
+      <p>"Completed a professional certification in Web Development Foundations by Cisco Networking Academy, covering semantic HTML5 structure, web accessibility standards, clean coding practices, and core document object model layout."</p>
+    </div>
+       <div>
+      <h3>Certificate OF HTML</h3>
+      <h5>Course By Cisco Networking Academy</h5>
+      <p>"Completed a professional certification in Web Development Foundations by Cisco Networking Academy, covering semantic HTML5 structure, web accessibility standards, clean coding practices, and core document object model layout."</p>
+    </div>
+    </>
   )
 }
 
