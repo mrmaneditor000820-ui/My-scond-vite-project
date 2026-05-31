99004f03789c3dc71438414a67c9f645886f8d76
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-31 10:03:21 +0500

    footer structure creating

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <>
-        <div>
-            <h1>Footer</h1>
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <h1>Abdul Rehman</h1>
+                <h5>Full Stack Developer</h5>
+            </div>
+            <div>
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                 <a href="#contact">Contact</a>
+            </div>
+            <hr />
+            <p>&copy; 2026 Abdul Rehman. All rights reserved.</p>
+            <div>
+                <a href="https://www.linkedin.com/in/abdul-rehman-9b8a1b1b3/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="https://github.com/abdulrehman" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </div>
+        </>
+    )
 }
 
-export default Footer
+export default Footer;
