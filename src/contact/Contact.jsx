7a113e2abc2c6eeecdab7ee4a483473c7cc5a831
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-31 08:25:00 +0500

    sturucture creating

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -6,9 +6,20 @@ function Contact() {
    <div>
     <h1>Get In Touch</h1>
    </div>
+   <div>
+    <form>
+      <label>Full Name</label>
+      <input type="text" placeholder='Name' required />
+      <label>Email</label>
+      <input type="email" placeholder='Email' required />
+      <label>Message</label>
+      <textarea placeholder='Message' required></textarea>
+      <button type='submit'>Send Message</button>
+    </form>
+   </div>
    
    </>
   )
 }
 
-export default Contact
+export default Contact;
