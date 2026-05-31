1da0ba80a5c20fb757673c88b00264dc47833877
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-31 08:30:18 +0500

    continus

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -17,7 +17,18 @@ function Contact() {
       <button type='submit'>Send Message</button>
     </form>
    </div>
-   
+   <div>
+    <h3>Contact Information</h3>
+    <h5>Email</h5>
+    <p>mrmaneditor000820@gmail.com</p>
+    <h5>Phone</h5>
+    <p>+923463419974</p>
+    <h5>Location</h5>
+    <p>Pakistan</p>
+    <h5>Connect With Me</h5>
+    <a href="https://github.com/mrmaneditor000820" target="_blank" rel="noopener noreferrer">GitHub</a>
+    <a href="https://www.linkedin.com/in/mrmaneditor000820/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+   </div>
    </>
   )
 }
