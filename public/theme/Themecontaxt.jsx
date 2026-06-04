dc7ff4aaa773efb6e40294750b689b1d569d920d
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-04 14:46:20 +0500

    use state creating

--- public/theme/Themecontaxt.jsx
+++ public/theme/Themecontaxt.jsx
@@ -2,17 +2,14 @@ import React, { useContext } from 'react'
 
 export const Theme = useContext()
 
-
-
-
 function Themecontaxt({children}) {
 
-
+const [first, setfirst] = useState(second)
     
 
   return (
-    <Theme.Provider>
-
+    <Theme.Provider >
+    {children}
     </Theme.Provider>
   )
 }
